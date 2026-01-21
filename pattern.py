5f7e732a866ab0b50b07c6d43e693bc11a17e1b9
pranayaknurwar <aknurwarpranay@gmail.com>
2026-01-21 19:56:11 +0530

    Logic Practice

---
+++ pattern.py
@@ -0,0 +1,21 @@
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end="  ")
+#     print("\n")
+
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print(j,end="  ")
+#     print("\n")
+
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print(i,end="  ")
+#     print("\n")
+
+for i in range(1,6):
+    for j in range(0,5-i):
+        print(" ",end="  ")
+    for k in range(0,2*i-1):
+        print("*",end="  ")
+    print()
